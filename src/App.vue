570352135dd2e19c35b8c63d122236a797673d99
VEZZO\alexv <alessandro.vezzoli@vp360.eu>
2024-02-19 21:21:03 +0100

    CREATION HEADER AND FOOTER

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,53 @@
+<template>
+  <header class="p-4 bg-color-black">
+    <div class="logo pr-3">
+      <img src="/Netflix-Symbol.png" alt="Logo" />
+    </div>
+    <div class="brand-text pl-3">BOOLFLIX</div>
+  </header>
+
+  <footer class="bg-color-black">
+    <p>CREATO DA VEZZO</p>
+  </footer>
+</template>
 <script setup></script>
 
-<template>
-  <h1>INIZIO PROGETTO</h1>
-</template>
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+}
 
-<style scoped></style>
+header {
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 10px;
+}
+
+footer {
+  color: #fff;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 105%;
+  height: 50px;
+  margin-left: -10px;
+}
+
+.bg-color-black {
+  background-color: #333;
+}
+.logo {
+  width: 50px;
+}
+
+.logo img {
+  width: 50px;
+}
+.brand-text {
+  font-size: 18px;
+}
+</style>
