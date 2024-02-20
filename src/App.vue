d376c0998aab695f827bad9856065c508ea45cd7
VEZZO\alexv <alessandro.vezzoli@vp360.eu>
2024-02-20 20:54:28 +0100

    chiamata api con realtive informazioni

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,10 @@
     <div class="brand-text pl-3">BOOLFLIX</div>
   </header>
   <main v-for="card in cards">
-    {{ card.original_title }}
+    Titolo film: {{ card.title }}<br />
+    Titolo originale film: ({{ card.original_title }})<br />
+    Lingua: {{ card.original_language }}<br />
+    Voto: {{ card.vote_average }} <br /><br />
   </main>
 
   <footer class="bg-color-black">
@@ -15,19 +18,30 @@
 </template>
 <script>
 import axios from "axios";
+
 export default {
   data() {
     return {
-      url: "https://api.themoviedb.org/3/search/movie?query=simpson&api_key=99c41b3e6bab796d423dcb20a0127b84",
+      url: "https://api.themoviedb.org/3/discover/movie?api_key=99c41b3e6bab796d423dcb20a0127b84",
       cards: [],
     };
   },
   mounted() {
-    axios.get(this.url).then((response) => {
-      console.log("ciao");
-      console.log(response.data.results);
-      this.cards = response.data.results;
-    });
+    const options = {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OWM0MWIzZTZiYWI3OTZkNDIzZGNiMjBhMDEyN2I4NCIsInN1YiI6IjYxZjUwNjgyNWYyZGIxMDBhMmE0NjlmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.epzWc8RGj-CpbtG1piX0gv8PPFwpxXKQoVkRJ7eFAyA",
+      },
+    };
+
+    axios
+      .get(this.url, options)
+      .then((response) => {
+        console.log("ciao");
+        console.log(response.data.results);
+        this.cards = response.data.results;
+      })
+      .catch((err) => console.error(err));
   },
 };
 </script>
